e29fae924e6837ff261d8b61cd590e816d8db942
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2024-03-01 14:38:04 +0530

    Create 3) if-else (Decision Making).cpp

---
+++ Step 1 - Basics/Step 1.1 - Introduction/3) if-else (Decision Making).cpp
@@ -0,0 +1,11 @@
+// https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235
+
+string compareIfElse(int a, int b)
+{
+	if (a > b)
+		return "greater";
+	else if (b > a)
+		return "smaller";
+	else
+		return "equal";
+}
