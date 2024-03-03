879ac1721e8bdd59f1c8075d179da369971ddbfd
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2024-03-03 21:50:35 +0530

    FA-1 ECE

---
+++ Step 1 - Basics/Step 1.1 - Introduction/7) FA-1 ECE.cpp
@@ -0,0 +1,37 @@
+// https://www.codingninjas.com/studio/problems/fa-1-ece_4606261
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int Maximum(int x, int y)
+{
+	return x > y ? x : y;
+}
+
+void Swap(int &x, int &y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+int main()
+{
+	int test, a, b, r;
+	cin >> test;
+	cin >> a >> b;
+	switch (test)
+	{
+	case 1:
+		r = Maximum(a, b);
+		cout << r;
+		break;
+	case 2:
+		Swap(a, b);
+		cout << a << " " << b;
+		break;
+	default:
+		cout << "Invalid test option";
+	}
+	return 0;
+}
