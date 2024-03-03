c49eb153c8efd59a568f6a86e600759b4e35d9c4
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2024-03-03 21:52:30 +0530

    Pattern 1

---
+++ Step 1 - Basics/Step 1.2 - Patterns/1) Pattern 1.cpp
@@ -0,0 +1,13 @@
+// https://www.codingninjas.com/studio/problems/n-forest_6570177
+
+void nForest(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
