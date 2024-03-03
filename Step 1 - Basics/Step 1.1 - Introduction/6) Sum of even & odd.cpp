76ee826278251736114de21b20087c793080d39c
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2024-03-03 21:49:39 +0530

    Sum of Even & Odd.

---
+++ Step 1 - Basics/Step 1.1 - Introduction/6) Sum of even & odd.cpp
@@ -0,0 +1,31 @@
+// https://www.codingninjas.com/studio/problems/sum-of-even-odd_624650
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int n;
+	cin >> n;
+
+	int even = 0;
+	int odd = 0;
+
+	while (n != 0)
+	{
+
+		int last = n % 10;
+		if (last % 2 == 1)
+		{
+			odd = odd + last;
+		}
+		else
+		{
+			even = even + last;
+		}
+		n = n / 10;
+	}
+
+	cout << even << " " << odd;
+}
