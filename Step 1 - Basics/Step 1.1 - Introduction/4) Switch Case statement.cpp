eaa53fbf90e6616d915a0a6166628203a52bbd31
Aryaman Tamotia <102788424+AryamanTamotia@users.noreply.github.com>
2024-03-03 21:48:15 +0530

    Switch Case

---
+++ Step 1 - Basics/Step 1.1 - Introduction/4) Switch Case statement.cpp
@@ -0,0 +1,12 @@
+// https://www.codingninjas.com/studio/problems/switch-case-statement_8357244
+
+double areaSwitchCase(int ch, vector<double> a)
+{
+	switch (ch)
+	{
+	case 1:
+		return M_PI * a[0] * a[0];
+	case 2:
+		return a[0] * a[1];
+	}
+}
