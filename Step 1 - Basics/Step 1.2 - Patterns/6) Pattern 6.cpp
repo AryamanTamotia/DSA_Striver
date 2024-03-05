eced59ac6334624daa274fd4aeb3953083511c40
Aryaman Tamotia <aryamantamotia@gmail.com>
2024-03-05 21:35:58 +0530

    Pattern 6

---
+++ Step 1 - Basics/Step 1.2 - Patterns/6) Pattern 6.cpp
@@ -0,0 +1,13 @@
+// https://www.codingninjas.com/studio/problems/reverse-number-triangle_6581889
+
+void nNumberTriangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
