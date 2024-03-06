6e027e066a585bdbd704b8eeda0ad41a90801276
Aryaman Tamotia <aryamantamotia@gmail.com>
2024-03-06 22:02:37 +0530

    Pattern 13

---
+++ Step 1 - Basics/Step 1.2 - Patterns/13) Pattern 13.cpp
@@ -0,0 +1,15 @@
+// https://www.codingninjas.com/studio/problems/increasing-number-triangle_6581893
+
+void nNumberTriangle(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+}
