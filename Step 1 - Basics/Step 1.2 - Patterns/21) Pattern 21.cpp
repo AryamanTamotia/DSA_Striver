f0441669859570cc81f91aa5e4942ae528c32bb8
Aryaman Tamotia <aryamantamotia@gmail.com>
2024-03-15 23:25:08 +0530

    Pattern 21

---
+++ Step 1 - Basics/Step 1.2 - Patterns/21) Pattern 21.cpp
@@ -0,0 +1,20 @@
+// https://www.codingninjas.com/studio/problems/ninja-and-the-star-pattern-i_6581920
+
+void getStarPattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == n || j == 1 || j == n)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
