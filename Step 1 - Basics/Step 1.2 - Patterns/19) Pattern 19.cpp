e84b7bbaf4e12640211609e535905c23f820acbe
Aryaman Tamotia <aryamantamotia@gmail.com>
2024-03-15 23:24:31 +0530

    Pattern 19

---
+++ Step 1 - Basics/Step 1.2 - Patterns/19) Pattern 19.cpp
@@ -0,0 +1,38 @@
+https : // www.codingninjas.com/studio/problems/symmetric-void_6581919
+
+        void
+        symmetry(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        for (int j = 1; j <= 2 * (i - 1); j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        for (int j = 1; j <= (n - i) * 2; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
